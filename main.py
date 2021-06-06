aeb505aeb71c8193588f83caa5e698d6cdb3ae2e
ymrTardes <ymr.tardes@gmail.com>
2021-06-06 16:48:36 +0300

    main

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import config
 bot = telebot.TeleBot(config.conf['TOKEN'])
 
 
-@bot.message_handlers(commands=['hello'])
+@bot.message_handler(commands=['hello'])
 def hello(message):
-    bot.send_message(message.chat.id, 'Привет я Cofemolka_bot, у меня вы можете ознакомиться с мееню и оставить отзыв :) Приятного аппетита!')
+    bot.send_message(message.chat.id, 'Привет я Cofemolka_bot, у меня вы можете ознакомиться с мееню и оставить отзыв :)\nПриятного аппетита!')
 
 
 bot.polling(none_stop=True)
