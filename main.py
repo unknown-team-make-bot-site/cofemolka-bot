67656e92c0032c82e6e35c9e595ff379293fd6fc
zhukofff <d3nj567@gmail.com>
2021-06-21 14:48:25 +0300

    add gitignore del cache

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ bot = telebot.TeleBot(config.conf['TOKEN'])
 
 @bot.message_handler(commands=['hello'])
 def hello(message):
-    bot.send_message(message.chat.id, 'Привет я Cofemolka_bot, у меня вы можете ознакомиться с мееню и оставить отзыв :)\nПриятного аппетита!')
+    bot.send_message(message.chat.id, 'Привет я Cofeemolka_bot, у меня вы можете ознакомиться с мееню '
+                                      'и оставить отзыв :)\nПриятного аппетита!')
 
 
 @bot.message_handler(commands=['menu'])
