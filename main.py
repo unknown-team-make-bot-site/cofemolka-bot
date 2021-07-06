f2d4ad478284b96ee672d29bf9f00a6ca0bcbf06
zhukofff <d3nj567@gmail.com>
2021-07-06 19:04:46 +0300

    some edit

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ bot = telebot.TeleBot(config.conf['TOKEN'])
 
 @bot.message_handler(commands=['hello'])
 def hello(message):
-    bot.send_message(message.chat.id, 'Привет я Cofeemolka_bot, у меня вы можете ознакомиться с мееню '
+    bot.send_message(message.chat.id, 'Привет я кофемолка, у меня вы можете ознакомиться с меню '
                                       'и оставить отзыв :)\nПриятного аппетита!')
 
 #database
-con = sql.connect('feddback.db')
+connect = sql.connect('feedback.db')
 
 @bot.message_handler(commands=['start'])
 def menu(message):
     markup = telebot.types.InlineKeyboardMarkup()
     markup.add(telebot.types.InlineKeyboardButton(text='Меню блюд епт', callback_data='food_menu'))
     markup.add(telebot.types.InlineKeyboardButton(text='Оставить отзыв', callback_data='feedback'))
-    bot.send_message(message.chat.id, 'Привет я Cofemolka_bot, у меня вы можете ознакомиться с мееню и оставить отзыв :)\nПриятного аппетита!')
+    hello(message)
     bot.send_message(message.chat.id, text='Выберете дейтвие:', reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -28,11 +28,13 @@ def query_handler(call):
         result = '1.\n 2.\n 3.'
     elif call.data == 'feedback':
         result = 'Оставьте отзыв:'
-        with con:
-        	cur = con.cursor()
-        	cur.execute("CREATE TABLE IF NOT EXITS `feedback` (`feed` STRING) ")
-        	con.commit()
+        with connect:
+            current = connect.cursor()
+            current.execute(""" CREATE TABLE IF NOT EXISTS feedback (
+        id integer PRIMARY KEY
+        description text NOT NULL);
+    """)
+    connect.commit()
     bot.send_message(call.message.chat.id, result)
     bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 bot.polling(none_stop=True)
-
